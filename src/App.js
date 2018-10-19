33ce62102815604e847262e66d3b4948dc384c73
Devin Mounts <devinmounts@gmail.com>
2018-10-19 11:35:20 -0700

    create bio component.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import Landing from './components/Landing';
 import Navigation from './components/Navigation';
+import Bio from './components/Bio';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
           <Navigation />
           <Landing />
+          <Bio />
       </div>
     );
   }
---
+++ src/components/Bio.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './../styles/Bio.css';
+
+const Bio = props => {
+  return(
+    <div>
+      <header className='header'>
+        About
+      </header>
+
+    </div>
+  );
+}
+
+export default Bio;
