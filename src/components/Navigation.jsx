993f57f6347baa0097a02d81c18f8c6f50dce0ad
Devin Mounts <devinmounts@gmail.com>
2018-10-19 09:50:12 -0700

    place navigation component.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,32 +6,20 @@ const Navigation = props => {
     <Navbar inverse collapseOnSelect>
       <Navbar.Header>
         <Navbar.Brand>
-          <a href="#brand">React-Bootstrap</a>
+          <a href="#brand">Devin Mounts</a>
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar.Header>
       <Navbar.Collapse>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            Link
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Link
-          </NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
-          </NavDropdown>
-        </Nav>
         <Nav pullRight>
           <NavItem eventKey={1} href="#">
-            Link Right
+            Bio
           </NavItem>
           <NavItem eventKey={2} href="#">
-            Link Right
+            Portfolio
+          </NavItem>
+          <NavItem eventKey={3} href="#">
+            Contact
           </NavItem>
         </Nav>
       </Navbar.Collapse>
