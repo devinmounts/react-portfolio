bddad4a80a53cc3a5dfdccf2ed69fd92d0023fda
Devin Mounts <devinmounts@gmail.com>
2018-10-19 17:09:18 -0700

    add switch statement for callbacks.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,63 @@ import Navigation from './components/Navigation';
 import Bio from './components/Bio';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import scrollToComponent from 'react-scroll-to-component';
+import PropTypes from 'prop-types';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.handleScrollToComponent = this.handleScrollToComponent.bind(this);
+    this.state = {
+      selectedComponent: null,
+      refComponent: null
+    }
+  }
+
+  handleScrollToComponent(component){
+    let newSelectedComponent;
+    switch (component) {
+      case 'this.Bio':
+        newSelectedComponent = this.Bio
+        return this.setState({
+          selectedComponent: newSelectedComponent
+        });
+        break;
+      case 'this.Portfolio':
+      newSelectedComponent = this.Portfolio
+      return this.setState({
+        selectedComponent: newSelectedComponent
+      });
+        break;
+      case 'this.Contact':
+      newSelectedComponent = this.Contact
+      return this.setState({
+        selectedComponent: newSelectedComponent
+      });
+        break;
+      default:
+
+  }
+
+    scrollToComponent(this.state.selectedComponent, {align: 'top'})
+  }
   render() {
+    console.log(this.state);
+    let component;
     return (
       <div>
-          <Navigation />
+          <Navigation onScrollToComponent={this.handleScrollToComponent} />
           <Landing />
-          <Bio />
-          <Portfolio />
-          <Contact />
+          <Bio ref={(component) => {this.Bio = component}}/>
+          <Portfolio ref={(component) => {this.Portfolio = component}}/>
+          <Contact ref={(component) => {this.Contact = component}}/>
       </div>
     );
   }
+}
+
+App.propTypes = {
+  onScrollToComponent: PropTypes.func
 }
 
 export default App;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,31 +1,50 @@
 import React from 'react'
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import './../styles/Navigation.css';
+import PropTypes from 'prop-types';
 
-const Navigation = props => {
-  return(
-    <Navbar inverse collapseOnSelect className='navigation'>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#brand">Devin Mounts</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav pullRight>
-          <NavItem eventKey={1} href="#">
-            Bio
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            Portfolio
-          </NavItem>
-          <NavItem eventKey={3} href="#">
-            Contact
-          </NavItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+class Navigation extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      component: null
+    }
+
+  }
+
+  handleScrollToComponent = (selected) => {
+    this.props.onScrollToComponent(selected);
+  }
+
+  render(){
+    return(
+      <Navbar inverse collapseOnSelect className='navigation'>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#brand">Devin Mounts</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav onSelect={this.handleScrollToComponent} pullRight>
+            <NavItem eventKey={'this.Bio'}>
+              Bio
+            </NavItem>
+            <NavItem eventKey={'this.Portfolio'}>
+              Portfolio
+            </NavItem>
+            <NavItem eventKey={'this.Contact'}>
+              Contact
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+Navigation.propTypes = {
+  onScrollToComponent: PropTypes.func
 }
 
 export default Navigation;
